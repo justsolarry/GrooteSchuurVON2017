cfcb9139f4f8836261847249cf49bff065911925
Denham Preen <denhampreen@gmail.com>
2017-09-07 12:40:26 +0200

    Merge branch 'Denham'
    Data transfer file integration, pull from database and post to test environment

--- js/transfer2018.js
+++ js/transfer2018.js
@@ -1,232 +1,29 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Test Environment</title>
-  </head>
-  <body>
-    <h1>Test Data Transfer Environment</h1>
-    <code id="xmlContent">
-    </code>
-    <div id="demo">text</div>
+//Notes:    Need to improve efficientcy by transferring multiple records in one xml file
+//          Must update database to reflect the records state of published
 
-<script>
-
-
-
-var babyData = {        //testData
-  patientMedicalRecordNumber: 14304791,
-  birthYear: 2018,
-  birthWeightInGrams: 900,
-  gestationalAgeInWeeks: 27,
-  gestationalAgePlusDays: 2,
-  diedInDeliveryRoom: 0,
-  outbornBirth: 1,
-  transferCenterCode: 99999999,
-  dayOfAdmission: 2,
-  headCircumferenceAtBirth: 23.8,
-  maternalRaceOfMother: 7,
-  prenatalCare: 1,
-  antenatalSteroids: 0,
-  antenatalMagnesiumSulfate: 0,
-  chorioamnionitis: 0,
-  maternalHypertension: 1,
-  maternalDiabetes: 0,
-  modeOfDelivery: 0,
-  sexOfInfant: 0,
-  multipleGestation: 0,
-  numberOfBirthsDelivered: 77,
-  congenitalInfection: 0,
-  congenitalInfectionOrg1: 7777,
-  congenitalInfectionOrg2: 7777,
-  congenitalInfectionOrg3: 7777,
-  apgarScoresOneMin: 7,
-  apgarScoresFiveMin: 9,
-  oxygenDuringInitialResuscitation: 1,
-  facemaskVentilation: 1,
-  LaryngealMaskAirway: 0,
-  endotrachealTubeVentilation:0,
-  epinephrine:0,
-  cardiacCompression:0,
-  nasalVentilation: 0,
-  nasalCpap: 0,
-  temperatureMeasuredAtAdmissionState: 1,
-  temperatureMeasuredAtAdmission: 35.9,
-  bacterialSepsis: 1,
-  bacterialSepsisPathogen1: 7777,
-  bacterialSepsisPathogen2: 7777,
-  bacterialSepsisPathogen3: 7777,
-  oxygenOnDay28: 1,
-  cranialImagning:1,
-  periventricularHemorrhage: 2,
-  pihOccurance: 1,
-  diedWithin12Hours: 0,
-  receivedOxygen: 1,
-  conventionalVentilation: 1,
-  highFrequencyVentilation: 0,
-  highFlowNasalCannula: 0,
-  nasalIMV: 0,
-  nasalCPAPafterResuscitation: 1,
-  nasalCPAPnoETT: 1,
-  surfactant: 0,
-  surfactantAtAnyTime: 1,
-  ageAtSurfactantInHours: 7,
-  ageAtSurfactantPlusMinutes: 0,
-  inhaledNitricOxide: 0,
-  inhaledNitricOxideWhereGiven: 7,
-  oxygenAt36Weeks: 1,
-  ventilationAt36Weeks: 0,
-  highFrequencyVentilationAt36Weeks: 0,
-  highFlowNasalCannulaAt36weeks: 1,
-  nasalIMVAt36Weeks: 0,
-  nasalCpapAt36Weeks: 0,
-  steroidsForCLD: 0,
-  steroidsForCLDWhereGiven: 7,
-  indomethacin: 0,
-  ibuprofen: 0,
-  Acetaminophen: 0,
-  probiotics: 1,
-  ropTreatmentWithVegfDrug: 0,
-  caffeine: 0,
-  vitaminA: 0,
-  ropSurgery: 0,
-  ropSurgeryWhereDone: 7,
-  pdaSurgery: 0,
-  necSurgery: 0,
-  otherSurgery: 0,
-  firstSurgeryCode: 77,
-  firstSurgeryLocation: 7,
-  secondSurgery: 77,
-  secondarySurgeryLocation: 7,
-  thirdSurgery: 77,
-  thirdSurgeryLocation: 7,
-  fourthSurgery: 77,
-  fourthSurgeryLocation: 7,
-  fifthSurgery: 77,
-  fifthSurgeryLocation: 7,
-  sixthSurgery: 77,
-  sixthSurgeryLocation: 7,
-  seventhSurgery: 77,
-  seventhSurgeryLocation: 7,
-  eighthSurgery: 77,
-  eighthSurgeryLocation: 7,
-  ninethSurgery: 77,
-  ninethSurgeryLocation: 7,
-  tenthSurgery: 77,
-  tenthSurgeryLocation: 7,
-  otherSurgeryDescription: 77,
-  respiratoryDistressSyndrome: 1,
-  pneumothorax: 0,
-  pneumothoraxWhereOccured: 7,
-  patentDuctusArteriosus: 1,
-  necrotizingEnterocolitis: 0,
-  necrotizingEnterocolitisWhereOccured: 7,
-  focalIntestinalPerforation: 0,
-  focalIntestinalPerforationWhereOccured: 7,
-  bacterialPathogenAfterDay3: 0,
-  bacterialPathogenAfterDay3WhereOccured: 7,
-  bacterialPathogenAfterDay3Pathogen1: 7777,
-  bacterialPathogenAfterDay3Pathogen2: 7777,
-  bacterialPathogenAfterDay3Pathogen3: 7777,
-  coagulaseNegativeStaphInfection: 1,
-  coagulaseNegativeStaphInfectionWhereOccured:1,
-  fungalInfection: 0,
-  fungalInfectionWhereOccured: 7,
-  cysticPeriventricularLeukomalacia: 0,
-  retinalExamination:1,
-  ropStage: 2,
-  majorBirthDefect: 0,
-  firstBirthDefectCode: 7777,
-  secondBirthDefectCode: 7777,
-  thirdBirthDefectCode: 7777,
-  fourthBirthDefectCode: 7777,
-  fifthBirthDefectCode: 7777,
-  birthDefectDescription: 77,
-  enteralfeeding: 3,
-  oxygenAtDischarge: 0,
-  conventionalVentilationAtDischarge: 0,
-  highFrequencyVentilationAtDischarge: 0,
-  highFlowNasalCannulaAtDischarge: 0,
-  nasalVentilationAtDischarge: 0,
-  nasalCpapAtDischarge: 0,
-  monitorAtDischarge: 0,
-  initialDisposition: 2,
-  weightAtInitialDisposition: 1000,
-  headCircumferenceAtInitialDisposition: 24.8,
-  initialLengthOfStay: 13,
-  reasonForTransfer: 1,
-  newTransferCenterCode: 77777777
-
-  // postTransferDisposition: 1,
-  // dispositionAfterReadmission: 7,
-  // weightAtDispositionAfterReadmission: 77777,
-  // ultimateDisposition: 7,
-  // totalLengthOfStay: 100,
-  // durationOfAssistedVentilation: 7,
-  // daysOfAssistedVentilation: 7777,
-  // ecmoAtHospital: 7,
-  // hypothermicTherapy: 0,
-  // coolingMethod: 7,
-  // hypoxicIschemicEncephalopathy: 7,
-  // hieSeverity: 7,
-  // meconiumAspirationSyndrome: 7,
-  // trachealSuctioningAttempted: 7,
-  // seizures: 7
-};
-
-// { maternalRaceOfMother
-//   undefined: 0,
-//   Black: 1,
-//   White: 3,
-//   Asian: 4,
-//   NativeAmerican: 5,
-//   PacificIslander: 6,
-//   Other: 7
-// },
-
-create2018Xml();
-
-function createRecord(){
-  createHTTPPOSTConnection(create2018Xml());
-}
-
-function deleteRecord(){
-  createHTTPPOSTConnection(deleteXmlData);
-}
-
-function updateRecord(){
-  createHTTPPOSTConnection(deleteXmlData);
-  createHTTPPOSTConnection(createXmlData);
-  }
-
-function createHTTPPOSTConnection(babyDataObject){
-  var http = new XMLHttpRequest();
-  var url = "http://196.42.86.86:5984/test1/3"; //server will change //test server https://www.posttestserver.com/
-  http.open("PUT", url, true);
-  http.setRequestHeader("Content-type", "application/json");
-  http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
+function sendDataToVon(listOfMedicalRecordIds){
+    for (i = 0; i < listOfMedicalRecordIds.length; i++) {
+        var record = getRecordFromDatabase(listOfMedicalRecordIds[i]);
+        var xmlRecord = create2018Xml(record);
+        transferXmlToVon(xmlRecord);
     }
-  }
-  document.getElementById("xmlContent").innerHTML = JSON.stringify(babyDataObject);
-  http.send(JSON.stringify(babyDataObject));
 }
 
-function createHTTPGETConnection(){
-  var http = new XMLHttpRequest();
-  var url = "http://196.42.86.86:5984/test1/3"; //admin:vonadmin123@
-  http.open("GET", url, false);
-  http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) {
-        babyData = this.responseText;
-        //console.log(this.responseText);
-        //document.getElementById("demo").innerHTML = babyData;
+function transferXmlToVon(xmlFile)
+{
+    var http = new XMLHttpRequest();
+    var url = "https://www.posttestserver.com/";
+    http.open("POST", url, true);
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            toastr.info("Data transfered succesfully  *To test environment");
+        }
     }
-  }
-  http.send();
+    http.send(xmlFile);
 }
 
-function create2018Xml (){ //passInJSON object data
+
+function create2018Xml (babyData){ //passInJSON object data
 
     // createHTTPGETConnection();
     //babyData = JSON.parse(babyData);
@@ -468,7 +265,4 @@ return xmlDocument;
 
 
 */
-</script>
 
-</body>
-</html>
