b0f3e8c47bac30cd3cb45112f4143e9b278be4ff
Larry Liu <justsolarry@hotmail.com>
2017-09-28 13:29:14 +0200

    Automated Date Counting

--- js/validationScript.js
+++ js/validationScript.js
@@ -319,6 +319,31 @@ $("#birthWeightInGrams").on( "keyup", function( event ) {
  
 } );
 
+$("#weightAtInitialDisposition").on( "keyup", function( event ) {
+    
+    // 1.
+    var selection = window.getSelection().toString();
+    if ( selection !== '' ) {
+        return;
+    }
+    // 2.
+    if ( $.inArray( event.keyCode, [38,40,37,39] ) !== -1 ) {
+        return;
+    }
+    // 1
+        var $this = $( this );
+        var input = $this.val();
+        // 2
+        var input = input.replace(/[\D\s\._\-]+/g, "");
+        // 3
+        input = input ? parseInt( input, 10 ) : 0;
+        // 4
+        $this.val( function() {
+            return ( input === 0 ) ? "" : input.toString();
+        } );
+ 
+} );
+
 $("#patientMedicalRecordNumber").on( "keyup", function( event ) {
     
     // 1.
