3484c7b209bf2b84cef5fdc9b72c4cacaec66bc5
Sabelo Xulu <cacofonixt@gmail.com>
2017-10-11 11:48:15 +0200

    Commiting rest

--- js/saveRecord.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("message", function(e){
-    importScripts('databaseReadWrite.js', 'config.js');
-    console.log(e.data);
-    console.log("In worker");
-    configureDB();
-    record = mapToNaturalLanguage(e.data);
-    rev = updateDataInRecord(record);
-    postMessage(rev);
-})
